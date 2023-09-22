dd140a01cdf8fa084d2de4e9f5458d9e0ff222e5
Sandipan Kalita <sandipankalitaofficial@gmail.com>
2023-09-22 17:49:25 +0530

    friend add remove bug fix

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -14,13 +14,17 @@ export const getUser = async (req, res) => {
 export const getUserFriends = async (req, res) => {
   try {
     const { id } = req.params;
-    const result = await User.findById(id).populate("friends");
-    const friends = result.friends.map(
+    const user = await User.findById(id);
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
       ({ _id, firstName, lastName, occupation, location, picturePath }) => {
         return { _id, firstName, lastName, occupation, location, picturePath };
       }
     );
-    res.status(200).json(friends);
+    res.status(200).json(formattedFriends);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
@@ -34,8 +38,8 @@ export const addRemoveFriend = async (req, res) => {
     const friend = await User.findById(friendId);
 
     if (user.friends.includes(friendId)) {
-      user.friends = user.friends.filter((id) => id !== friendId);
-      friend.friends = friend.friends.filter((id) => id !== id);
+      user.friends = user.friends.filter((id) => id != friendId);
+      friend.friends = friend.friends.filter((id) => id != id);
     } else {
       user.friends.push(friendId);
       friend.friends.push(id);
